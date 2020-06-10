84771289fcd1cecf2606c0447909ad5e764b2b16
Chelle <chellecol@gmail.com>
2020-06-10 17:01:42 -0400

    "changed to using css instead of javascript"

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const form = document.querySelector('#memeForm');
 const imgForm = document.querySelector("#website");
 const topTextForm = document.querySelector("#tpText");
 const bottomTextForm = document.querySelector("#btText");
-var imH;
-var imW;
 
 list.addEventListener('click', function(evt){
     if(evt.target.tagName === 'BUTTON'){
         evt.target.parentElement.remove();
-        
     }
 })
 
@@ -26,68 +23,32 @@ form.addEventListener('submit', function(evt){
 
 
 function createMeme(img, tpText, btText){
-    let li = document.createElement('li');
-    let i = createMemeImg(img);
+    let div = document.createElement('div');
+    div.style.backgroundImage = `url(${img})`;
     let tt = createMemeTpText(tpText)
+
     let rmvBtn = document.createElement('button');
     rmvBtn.innerText = 'Remove Meme';
-    
-    li.appendChild(i);   //add the image and text to the list of Memes
-    li.appendChild(tt);
-    list.appendChild(li); //add to the page
+    rmvBtn.classList.add('buttonStyle');
 
+    div.appendChild(tt);
+    list.appendChild(div); //add to the page
 
-    let bt = createMemeBtText(btText, tt.offsetWidth);  //then create bottom text. 
-    li.appendChild(bt);      //so we can use the width of the top to offset the bottom text
-    li.appendChild(rmvBtn);
-}
-
-function createMemeImg(imgString){
-    let img = document.createElement("img");
-    img.setAttribute("src", imgString);
-        //resize image
-    imW = img.naturalWidth;
-    imH = img.naturalHeight;
-        //resize longest of two sides to 400px -> smaller side proportionally
-    if(imW>imH){  //w is longest side
-        imH = (imH*300)/imW;
-        imW = 300;
-    }else{
-        imW = (imW*300)/imH;
-        imH = 300;
-    }
-        //set the dimentions of the image
-    img.width = imW; 
-    img.height = imH;
-
-    img.classList.add('relativeImg');
-    return img;
+    let bottomText = createMemeBtText(btText);
+    div.appendChild(bottomText);
+    div.appendChild(rmvBtn);
 }
 
 function createMemeTpText(text){
     let span = document.createElement('span')
     span.innerText = text;
-    span.classList.add('relativeTextTop');
-
-    tempSpace.appendChild(span); //add span to html to get offsetwidth
-    let w = span.offsetWidth;
-    let half = (imW + w)/2;      //math to find where to start for center
-
-    span.style.left = `-${half}px`;
-    span.style.top = `-${imH - 20}px`;
-
+    span.classList.add('topText');
     return span;
 }
 
 function createMemeBtText(text, num){
     let span = document.createElement('span')
     span.innerText = text;
-    span.classList.add('relativeTextBottom'); 
-    
-    tempSpace.appendChild(span); //add span to html to get offsetwidth
-    let w = span.offsetWidth;
-    let half = (imW + w)/2; //math to find where to start for center
-    span.style.left = `-${half + num}px`;//add the length of the origonal string
-
+    span.classList.add('bottomText'); 
     return span;
 }
